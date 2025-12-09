f1cb1a7dddb2ef8cf617f96a816e24519e572e81
Israel Almeida <almeidev.rael@gmail.com>
2025-12-09 17:09:45 -0300

    Desenvolvimento completo

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let image = document.querySelector(".starbucks_cup")
+let back = document.querySelector(".circle")
 
 function changeImage(address){
     image.src = address
+}
+
+function changeBackground(color) {
+    back.style.background = color
 }
